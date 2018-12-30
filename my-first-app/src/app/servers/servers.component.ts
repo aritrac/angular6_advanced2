2ca29db40c97394480b6337544711103caa8ceff
aritrachatterjee2007@gmail.com <aritrachatterjee2007@gmail.com>
2018-12-30 19:57:19 +0530

    Before practicing components assignment

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -1,12 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
+  // attribute selector
+  // selector: '[app-servers]',
+
+  // class selector
+  // selector: '.app-servers',
+
+  // element selector
   selector: 'app-servers',
+
   // external template file
   // templateUrl: './servers.component.html',
-  // inline template
-  template: `<app-server></app-server>
-             <app-server></app-server>`,
+
+  // inline template for element selector
+     template: `<app-server></app-server>
+                <app-server></app-server>`,
+
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
