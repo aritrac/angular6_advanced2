ec63bc4910173924a301e9477c9f037ad5c70241
aritrachatterjee2007@gmail.com <aritrachatterjee2007@gmail.com>
2018-12-30 20:35:11 +0530

    Adding practice components solution

---
+++ assgn-practice-components/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { WarningAlertComponent } from './warningalert/warningalert.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    WarningAlertComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
